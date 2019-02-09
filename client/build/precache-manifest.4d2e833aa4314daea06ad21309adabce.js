47465934e35810d5da5ee765274570e6d4481946
ankitjena <ankitjena13@gmail.com>
2019-02-09 20:42:55 +0530

    readd client

---
+++ client/build/precache-manifest.4d2e833aa4314daea06ad21309adabce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "40d0597ac48bcecc66ed",
+    "url": "/static/js/main.40d0597a.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "40d0597ac48bcecc66ed",
+    "url": "/static/css/main.dd1b39bd.chunk.css"
+  },
+  {
+    "revision": "2367604c1c18ffdc5214ad6ba6b4b9dd",
+    "url": "/index.html"
+  }
+];
